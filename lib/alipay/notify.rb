c90ee08682bb4e9e5c7aec2d5a13045d02814a18
zhangxiaohua <445679586@qq.com>
2014-11-12 14:41:58 +0800

    将 delete params['partner'] 放到 stringify_keys 之后，这样不管传递进来的参数是 symbol 还是 string key 都可以兼容

--- lib/alipay/notify.rb
+++ lib/alipay/notify.rb
@@ -2,8 +2,8 @@ module Alipay
   module Notify
     module Wap
       def self.verify?(params)
-        partner = params.delete['partner']
         params = Utils.stringify_keys(params)
+        partner = params.delete['partner']
         notify_id = params['notify_data'].scan(/\<notify_id\>(.*)\<\/notify_id\>/).flatten.first
 
         Sign::Wap.verify?(params) && Notify.verify_notify_id?(notify_id, partner)
@@ -12,15 +12,15 @@ module Alipay
 
     module App
       def self.verify?(params)
-        partner = params.delete['partner']
         params = Utils.stringify_keys(params)
+        partner = params.delete['partner']
         Sign::App.verify?(params) && Notify.verify_notify_id?(params['notify_id'], partner)
       end
     end
 
     def self.verify?(params)
-      partner = params.delete['partner']
       params = Utils.stringify_keys(params)
+      partner = params.delete['partner']
       Sign.verify?(params) && verify_notify_id?(params['notify_id'], partner)
     end
 
